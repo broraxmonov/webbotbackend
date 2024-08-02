33123affc85cc67fbb82d2f6c79257f5dd67b189
broraxmonov <bro.raxmonov@gmail.com>
2024-08-03 01:29:53 +0500

    push

---
+++ index.js
@@ -0,0 +1,125 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+const token = '6555626821:AAEWM4uwFIA4m272kXIPK870P3P7oj6oYlw';
+
+// const token = '6392124644:AAGweSFNWmhx3pOXXRLlcFUTgb_tGpS2Sag';
+
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.setMyCommands([
+	{ command: '/start', description: "Kurslar haqida ma'lumot" },
+	{ command: '/courses', description: 'Barcha kurslar' },
+]);
+
+bot.on('message', async msg => {
+	const chatId = msg.chat.id;
+	const text = msg.text;
+
+	if (text === '/start') {
+		await bot.sendMessage(
+			chatId,
+			'Hems coin earn and play',
+			{
+				reply_markup: {
+					inline_keyboard: [
+						[
+							{
+								text: "Hems Coin Earn",
+								web_app: {
+									url: 'https://hemscoinwebbot-rxih.vercel.app/',
+								},
+							},
+						],
+					],
+				},
+			}
+		);
+	}
+
+	if (text === '/courses') {
+		await bot.sendMessage(
+			chatId,
+			'Sammi.ac platformasida bor kurslarni sotib olishingiz mumkin',
+			{
+				reply_markup: {
+					inline_keyboard: [
+						[
+							{
+								text: "Kurslarni ko'rish",
+								web_app: {
+									url: 'https://telegram-web-bot.vercel.app',
+								},
+							},
+						],
+					],
+				},
+			}
+		);
+	}
+
+	if (msg.web_app_data?.data) {
+		try {
+			const data = JSON.parse(msg.web_app_data?.data);
+
+			await bot.sendMessage(
+				chatId,
+				"Bizga ishonch bildirganingiz uchun raxmat, siz sotib olgan kurslarni ro'yhati"
+			);
+
+			for (item of data) {
+				await bot.sendPhoto(chatId, item.Image);
+				await bot.sendMessage(
+					chatId,
+					`${item.title} - ${item.quantity}x`
+				);
+			}
+
+			await bot.sendMessage(
+				chatId,
+				`Umumiy narx - ${data
+					.reduce((a, c) => a + c.price * c.quantity, 0)
+					.toLocaleString('en-US', {
+						style: 'currency',
+						currency: 'USD',
+					})}`
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+});
+
+app.post('/web-data', async (req, res) => {
+	const { queryID, products } = req.body;
+
+	try {
+		await bot.answerWebAppQuery(queryID, {
+			type: 'article',
+			id: queryID,
+			title: 'Muvaffaqiyatli xarid qildingiz',
+			input_message_content: {
+				message_text: `Xaridingiz bilan tabriklayman, siz ${products
+					.reduce((a, c) => a + c.price * c.quantity, 0)
+					.toLocaleString('en-US', {
+						style: 'currency',
+						currency: 'USD',
+					})} qiymatga ega mahsulot sotib oldingiz, ${products
+					.map(c => `${c.title} ${c.quantity}X`)
+					.join(', ')}`,
+			},
+		});
+		return res.status(200).json({});
+	} catch (error) {
+		return res.status(500).json({});
+	}
+});
+
+app.listen(process.env.PORT || 8000, () =>
+	console.log('Server started')
+);
